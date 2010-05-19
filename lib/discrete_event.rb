a2ac1dd78ba120d39fa13adffa179b5d0b328909
John Lees-Miller <jdleesmiller@gmail.com>
2010-05-19 23:18:51 +0100

    added shorthand for creating an instance of Simulation

--- lib/discrete_event.rb
+++ lib/discrete_event.rb
@@ -3,3 +3,10 @@ require 'pqueue'
 require 'discrete_event/simulation'
 require 'discrete_event/fake_rand'
 
+module DiscreteEvent
+  def self.simulation *args, &block 
+    sim = DiscreteEvent::Simulation.new(*args)
+    sim.instance_eval(&block)
+    sim
+  end
+end
