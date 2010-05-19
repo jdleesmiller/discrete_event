9d9a249fe6d2676c0794c3697999f7ecd7127455
John Lees-Miller <jdleesmiller@gmail.com>
2010-05-19 23:19:28 +0100

    added at_each_index, every, and recur_after

--- lib/discrete_event/simulation.rb
+++ lib/discrete_event/simulation.rb
@@ -5,36 +5,41 @@ module DiscreteEvent
   # There are two key terms:
   # * action: any Ruby block
   # * event: an action to be executed at some specified time in the future
-  # Events are created using the {#at} and {#after} methods. 
+  # Events are usually created using the {#at} and {#after} methods.
+  # The methods {#at_each_index}, {#every} and {#recur_after} make some
+  # important special cases more efficient.
+  #
+  # See the {file:README} for an example.
   #
   # Typically, you will want to inherit from this class to construct a
   # simulation of your own. However, you can also use it as a standalone event
   # queue.
   #
   class Simulation
-    # @private
+    #
+    # Event queue entry for events; you do not need to use this class directly.
+    #
     Event = Struct.new(:time, :action)
-    class Event
-      def > rhs; self.time < rhs.time end
-    end
 
-    # Current time; from the currently executing event.
+    # Current time (taken from the currently executing event, if any).
     # @return [Number]
     attr_reader :now
 
-    # Event queue; usually, you shouldn't change this.
+    # Event queue.
     # @return [PQueue]
     attr_reader :events
+    protected :events
 
     def initialize start_time=0.0
       @start_time = start_time
       @now = start_time
-      @events = PQueue.new
+      @events = PQueue.new {|a,b| a.time < b.time}
+      @recur_interval = nil
     end
 
     # Schedule +action+ (a block) to run at the given +time+.
     #
-    # @param [Number] time at which +action+ should run; must be >= #{now}
+    # @param [Number] time at which +action+ should run; must be >= {#now}
     # @yield [] action to be run at +time+
     # @return [nil]
     def at time, &action
@@ -45,11 +50,103 @@ module DiscreteEvent
 
     # Schedule +action+ (a block) to run after the given +delay+ (with respect
     # to {#now}).
+    #
     # @param [Number] delay after which +action+ should run; non-negative
     # @yield [] action to be run after +delay+
     # @return [nil]
     def after delay, &action
       at(@now + delay, &action)
+    end
+
+    # Schedule +action+ (a block) to run at each time in +times+; the index
+    # into times is given to the +action+ block.
+    #
+    # This method may be of interest if you have a large number of events that
+    # occur at known times. You could use {#at} to add each one to the event
+    # queue at the start of the simulation, but this will make adding other
+    # events more expensive. Instead, this method adds them one at a time, so
+    # only the next event is stored in the event queue.
+    #
+    # @example
+    #   Alert = Struct.new(:when, :message)
+    #   alerts = [Alert.new(12, "ha!"), Alert.new(42, "ah!")] # and many more
+    #   at_each_index alerts.map{|a| a.when} do |i|
+    #     puts alerts[i].message
+    #   end
+    # 
+    # @param [Array<Numeric>] times to yield at; must be non-decreasing; you
+    #                         will usually want +times+ to stay the same for the
+    #                         duration of the simulation (pass a copy if you
+    #                         plan to change the original object).
+    # @param [Fixnum] i start at this index in +times+
+    # @return [nil]
+    def at_each_index times, index=0, &action
+      return nil unless times[index]
+      at times[index] do
+        yield(index)
+        at_each_index(times, index+1, &action)
+      end
+      nil
+    end
+
+    # When called from within an action block, repeats the action block after
+    # the specified +interval+ has elapsed.
+    #
+    # Calling this method from outside an action block has no effect.
+    # You may call this method at most once in an action block.
+    #
+    # @example
+    #   at 5 do
+    #     puts "now: #{now}"
+    #     recur_after 10*rand
+    #   end
+    #
+    # Note that you can achieve the same effect using {#at} and {#after} and a
+    # named method, as in
+    #   def demo
+    #     at 5 do
+    #       puts "now: #{now}"
+    #       after 10*rand do
+    #         demo
+    #       end
+    #     end
+    #   end
+    # but it is somewhat more efficient to call +recur_after+, and, if you do,
+    # the named method is not necessary.
+    #
+    # @param [Numeric] interval non-negative
+    # @return [nil]
+    def recur_after interval
+      raise "cannot recur twice" if @recur_interval
+      @recur_interval = interval
+      nil
+    end
+
+    # Schedule +action+ (a block) to run periodically.
+    #
+    # This is useful for statistics collection.
+    #
+    # Note that if you specify one or more events of this kind, the simulation
+    # will never run out of events, so you have to manually stop it (usually by
+    # throwing <tt>:stop</tt>).
+    #
+    # @example
+    #   every 5 do
+    #     if now > 100
+    #       # record stats
+    #     end
+    #     throw :stop if now > 1000
+    #   end
+    #
+    # @param [Numeric] interval non-negative
+    # @param [Numeric] start block first runs at this time
+    # @return [nil]
+    def every interval, start=0, &action
+      at start do
+        action.call
+        recur_after interval
+      end
+      nil
     end
 
     # Clear any pending events in the event queue and reset {#now}.
@@ -65,6 +162,7 @@ module DiscreteEvent
 
     # Called by +run+ when beginning a new simulation; you will probably want
     # to override this.
+    #
     # @abstract
     def start
     end
@@ -73,13 +171,28 @@ module DiscreteEvent
     # Run the action for the next event in the queue.
     #
     # @yield [] after the event (if any) runs
-    # @return [Boolean] false if there are no more steps.
+    # @return [Boolean] false if there are no more events.
     #
     def update &block
-      event = @events.pop
+      event = @events.top
       return false unless event
+
+      # Perform the action.
       @now = event.time
       event.action.call
+
+      # Recurring events get special treatment: can avoid doing a push and a pop
+      # by reusing the Event at the top of the heap, but with a new time. Note
+      # that this code is wrong if there is a new top element in the heap, but
+      # this should not be possible.
+      if @recur_interval
+        event.time = @now + @recur_interval
+        @events.replace_top(event)
+        @recur_interval = nil
+      else
+        @events.pop
+      end
+
       block.call if block_given?
       return true
     end
--- test/test_discrete_event.rb
+++ test/test_discrete_event.rb
@@ -40,6 +40,25 @@ class TestDiscreteEvent < Test::Unit::TestCase
     c.reset.run # no exception
     assert_equal 3, c.consumed.size
   end
+
+  def test_fake_rand_n
+    # Test that we can also fake random integers (for Kernel::rand(n)).
+    o = Object.new
+    class <<o
+      def test
+        rand(11)
+      end
+    end
+
+    FakeRand.for(o, 0.0, 0.1, 0.5, 0.99)
+    assert_equal 0, o.test
+    assert_equal 1, o.test
+    assert_equal 5, o.test
+    assert_equal 10, o.test
+
+    # Now have run out of fakes.
+    assert_raise(RuntimeError){ o.test }
+  end
   
   def test_mm1_queue_not_busy
     # Service begins immediately when queue is not busy.
@@ -79,10 +98,71 @@ class TestDiscreteEvent < Test::Unit::TestCase
     assert_near 0.4,  q.served[2].service_begin
     assert_near 0.5,  q.served[2].service_end
   end
+
+  def test_recur_after
+    output = []
+    DiscreteEvent.simulation {
+      at 0 do
+        output << now
+        recur_after 5 if now < 20
+      end
+
+      run
+    }
+    assert_equal [0, 5, 10, 15, 20], output
+  end
+
+  def test_recur_after_with_after_0
+    # Putting a new event in the queue and then calling recur_after should not
+    # displace the root element, even if you call after(0), which is just an
+    # edge case anyway.
+    output = []
+    DiscreteEvent.simulation {
+      at 0 do
+        output << now
+        after 0 do
+          output << 42
+        end
+        after 1 do
+          output << 13
+        end
+        recur_after 5 if now < 10
+      end
+
+      run
+    }
+    assert_equal [0, 42, 13, 5, 42, 13, 10, 42, 13], output
+  end
+
+  def test_every
+    output = []
+    DiscreteEvent.simulation {
+      every 3 do
+        output << now
+        throw :stop if now > 10
+      end
+      run
+    }
+    assert_equal [0,3,6,9,12], output
+  end
+
+  Alert = Struct.new(:when, :message)
+
+  def test_at_each_index
+    output = []
+    DiscreteEvent.simulation {
+      alerts = [Alert.new(12, "ha!"), Alert.new(42, "ah!")] # and many more
+      at_each_index alerts.map{|a| a.when} do |i|
+        output << now << alerts[i].message
+      end
+      run
+    }
+    assert_equal [12, 'ha!', 42, 'ah!'], output
+  end
   
   def test_mm1_queue_demo
-    # Just run the demo... 10000 isn't enough to get a reliable average.
-    obs_q, exp_q, obs_w, exp_w= mm1_queue_demo(0.25, 0.5, 10000)
+    # Just run the demo... 1000 isn't enough to get a reliable average.
+    obs_q, exp_q, obs_w, exp_w= mm1_queue_demo(0.25, 0.5, 1000)
     assert_near exp_q, 0.5   # mean queue = rho^2 / (1 - rho)
     assert_near exp_w, 2.0   # mean wait  = rho / (mu - lambda)
   end
