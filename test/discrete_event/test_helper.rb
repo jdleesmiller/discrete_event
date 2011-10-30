eb7a45c6acdb79e9b54a63bee32c51c1c2bbeae6
John Lees-Miller <jdleesmiller@gmail.com>
2011-10-30 13:06:20 +0000

    removed load path manip

--- test/discrete_event/test_helper.rb
+++ test/discrete_event/test_helper.rb
@@ -1,8 +1,4 @@
-require 'rubygems'
 require 'test/unit'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'discrete_event'
 
 class Test::Unit::TestCase
