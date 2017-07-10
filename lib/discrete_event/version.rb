21084c4eee5ba5c3c7e13dbc7faf3c78101df28c
John Lees-Miller <jdleesmiller@gmail.com>
2017-07-10 21:43:31 +0100

    Bump version to 2.0.0

--- lib/discrete_event/version.rb
+++ lib/discrete_event/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module DiscreteEvent
-  VERSION_MAJOR = 1
-  VERSION_MINOR = 1
+  VERSION_MAJOR = 2
+  VERSION_MINOR = 0
   VERSION_PATCH = 0
   VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
 end
